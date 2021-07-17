7e1b9edd2eb82fb92029784cb3210e383830ae4f
Vukovic Emilie <emilievukovic79@gmail.com>
2021-07-17 19:32:29 +0200

    [tests unitaires] - Start containers/Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,18 +1,45 @@
 import { fireEvent, screen } from '@testing-library/dom';
 import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
+import Bills from '../containers/Bills.js';
+import Router from '../app/Router';
+import { ROUTES, ROUTES_PATH } from '../constants/routes';
 import userEvent from '@testing-library/user-event';
+import Firestore from '../app/Firestore';
+import firebase from '../__mocks__/firebase';
+import { localStorageMock } from '../__mocks__/localStorage.js';
 
 describe('Given I am connected as an employee', () => {
     describe('When I am on Bills Page', () => {
         test('Then bill icon in vertical layout should be highlighted', () => {
-            const html = BillsUI({ data: [] });
-            document.body.innerHTML = html;
-            // to-do write expect expression
+            jest.mock('../app/Firestore');
+            Firestore.bills = () => ({
+                bills,
+                get: jest.fn().mockResolvedValue(),
+            });
+            Object.defineProperty(window, 'localStorage', {
+                value: localStorageMock,
+            });
+            window.localStorage.setItem(
+                'user',
+                JSON.stringify({
+                    type: 'Employee',
+                })
+            );
+            const pathname = ROUTES_PATH['Bills'];
+            Object.defineProperty(window, 'location', {
+                value: { hash: pathname },
+            });
+            document.body.innerHTML = `<div id="root"></div>`;
+            Router();
+            expect(
+                screen
+                    .getByTestId('icon-window')
+                    .classList.contains('active-icon')
+            ).toBeTruthy();
         });
 
         test('Then bills should be ordered from earliest to latest', () => {
-            // const html = BillsUI({ data: bills });
             const billsRended = [...bills];
             billsRended.map(bill => {
                 bill.formatedDate = bill.date;
@@ -24,7 +51,6 @@ describe('Given I am connected as an employee', () => {
                     /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
                 )
                 .map(a => a.innerHTML);
-            // /^((20[0-1]+[0-9]|202[0-1])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](20[0-1]+[0-9]|202[0-1]))$/
             const antiChrono = (a, b) => (a < b ? 1 : -1);
             const datesSorted = [...dates].sort(antiChrono);
             expect(dates).toEqual(datesSorted);
@@ -36,10 +62,39 @@ describe('Given I am connected as an employee', () => {
             expect(screen.getAllByText('Loading...')).toBeTruthy();
         });
 
-        test('Then error page should be rendered', () => {
+        test('Then if error page should be rendered', () => {
             const html = BillsUI({ error: 'error' });
             document.body.innerHTML = html;
             expect(screen.getAllByText('Erreur')).toBeTruthy();
+        });
+
+        describe('When I click on the eye icon', () => {
+            test('Then a modal should be open', () => {
+                const html = BillsUI({ data: bills });
+                document.body.innerHTML = html;
+                const onNavigate = pathname => {
+                    document.body.innerHTML = ROUTES({ pathname });
+                };
+                const bill = new Bills({
+                    document,
+                    onNavigate,
+                    firestore: null,
+                    localStorage: window.localStorage,
+                });
+                $.fn.modal = jest.fn();
+                const iconEye = screen.getAllByTestId('icon-eye')[0];
+                const handleClickIconEye = jest.fn(e => {
+                    e.preventDefault();
+                    bill.handleClickIconEye(iconEye);
+                });
+                iconEye.addEventListener('click', handleClickIconEye);
+                fireEvent.click(iconEye);
+                expect(handleClickIconEye).toHaveBeenCalled();
+            });
+        });
+
+        describe('When I click on the "New Bill" button', () => {
+            test('Then NewBill page should be rendered', () => {});
         });
     });
 });
